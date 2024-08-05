fd3a9286fe40b968de3c2e4b85f6ec9079c44278
osoken <osoken@osoekawalab.com>
2024-08-05 23:54:37 +0900

    feat: export some components

--- src/olt2t/__init__.py
+++ src/olt2t/__init__.py
@@ -2,7 +2,15 @@ __version__ = "0.1.0"
 
 
 from .core import TextToTextCore
-from .models import StrT
+from .models import ChatRoleType, ChatTask, ChatTurn, StrT, SummarizationTask
 from .settings import TextToTextCoreSettings
 
-__all__ = ["TextToTextCore", "TextToTextCoreSettings", "StrT"]
+__all__ = [
+    "TextToTextCore",
+    "TextToTextCoreSettings",
+    "StrT",
+    "SummarizationTask",
+    "ChatTask",
+    "ChatRoleType",
+    "ChatTurn",
+]
